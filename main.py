829c9eb785bb1644ddc74801b485cde256553318
Ding Jinghui <dingjinghui33@163.com>
2025-04-01 20:50:38 +0800

    feat: 图片转 bit map

--- main.py
+++ main.py
@@ -8,36 +8,29 @@ def resize_image(img, base_width):
     return img
 
 
-def binaries(img, threshold, invert=False):
+def binaries(img, threshold):
     # Grayscale
     img_gray = img.convert('L')
     img_gray = img_gray.point(lambda p: 255 if p > threshold else 0)
 
-    # Invert colors if needed
-    if invert:
-        img_gray = img_gray.point(lambda p: 255 - p)
+    # 预览灰度图
+    img_gray.show()
 
-    # To mono
-    res_img = img_gray.convert('1')
-    return res_img
-
-def to_array(img):
+    img_gray_data = list(img_gray.getdata())
+    res = []
+    for v in img_gray_data:
+        if v == 255:
+            res.append(1)
+        else:
+            res.append(0)
     width, height = img.size
-    pixels = list(img.getdata())
 
-    bit_map = []
-    for y in range(0, height, 8):  # 每次处理 8 行
-        for x in range(width):
-            byte = 0
-            for bit in range(8):
-                if y + bit < height and pixels[(y + bit) * width + x] == 0:  # 黑色像素
-                    byte |= (1 << bit)
-            bit_map.append(byte)
     return {
         'width': width,
         'height': height,
-        'bit_map': bit_map,
+        'bit_map': res
     }
+
 
 def get_c_array_str(bit_map_info):
     width = bit_map_info['width']
@@ -46,21 +39,27 @@ def get_c_array_str(bit_map_info):
 
     size_info = f'_{width}x{height}'
 
-    c_array = "const unsigned char bit_map" + size_info + "[] = {\n"
-    c_array += ", ".join(f"0x{b:02X}" for b in bit_map)
+    byte_array = []
+    # 每 8 个元素组成一个字节
+    for i in range(0, len(bit_map), 8):
+        chunk = bit_map[i:i+8]
+        byte_value = 0
+        for bit in chunk:
+            byte_value = (byte_value << 1) | bit
+        byte_array.append(byte_value)
+
+    c_array = "const unsigned char bit_map" + size_info + "[] = {\n  "
+    c_array += ", ".join(f"0x{b:02X}" for b in byte_array)
     c_array += "\n};"
 
     return c_array
 
 def main():
-    img = Image.open('./test_img/1.jpg')
+    img = Image.open('./test_img/8.jpg')
     print(f'{img.format} - {img.size} - {img.mode}')
     img = resize_image(img, base_width=64)
-    res = binaries(img, threshold=200, invert=True)
-    res.show()
-    bit_map = to_array(res)
-    print(bit_map)
-    c_array = get_c_array_str(bit_map)
+    res = binaries(img, threshold=180)
+    c_array = get_c_array_str(res)
     print(c_array)
 
 
