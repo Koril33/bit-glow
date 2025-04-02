a0d7636e31b876ede782dad5f862eb73cdf720d2
dingjinghui <dingjinghui33@163.com>
2025-04-02 09:12:16 +0800

    perf: 添加注释

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from PIL import Image
 
-
 def resize_image(img, base_width):
+    """
+    根据给定宽度，等比缩放图片
+    :param img: PIL image
+    :param base_width: 给定的宽度
+    :return: 缩放后的 PIL image
+    """
     width_percent = (base_width / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(width_percent)))
     img = img.resize((base_width, hsize), Image.Resampling.LANCZOS)
@@ -9,6 +14,12 @@ def resize_image(img, base_width):
 
 
 def binaries(img, threshold):
+    """
+    将图片转成 0 和 1 组成的数组
+    :param img: PIL image
+    :param threshold: 阈值
+    :return: 返回图片以及宽和高
+    """
     # Grayscale
     img_gray = img.convert('L')
     img_gray = img_gray.point(lambda p: 255 if p > threshold else 0)
@@ -33,6 +44,12 @@ def binaries(img, threshold):
 
 
 def get_c_array_str(bit_map_info):
+    """
+    转成 C 数组格式
+    :param bit_map_info: 经过 binary 函数得到的对象
+    :return: C 数组格式的字符串
+    """
+
     width = bit_map_info['width']
     height = bit_map_info['height']
     bit_map = bit_map_info['bit_map']
@@ -48,6 +65,7 @@ def get_c_array_str(bit_map_info):
             byte_value = (byte_value << 1) | bit
         byte_array.append(byte_value)
 
+    # 转换成 C 数组的字符串形式
     c_array = "const unsigned char bit_map" + size_info + "[] = {\n  "
     c_array += ", ".join(f"0x{b:02X}" for b in byte_array)
     c_array += "\n};"
